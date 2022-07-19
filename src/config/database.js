722eabfcdf313e36f698db672bcf897c1f4350b2
Sebastian Varona <sebastianvarona@gmail.com>
2022-07-19 16:45:52 -0500

    added method to create the table if not exists when initialized the server

--- src/config/database.js
+++ src/config/database.js
@@ -3,4 +3,17 @@ sqlite3.verbose();
 
 const database = () => new sqlite3.Database('src/db/database.db');
 
+export const initializeDatabase = () => {
+  const db = database();
+  db.run(`CREATE TABLE IF NOT EXISTS data (
+		id INTEGER PRIMARY KEY,
+		concepto varchar(255) not null,
+		monto double(10,2) not null,
+		fecha char(10) not null,
+		tipo varchar(10) not null,
+		categoria varchar(255)
+	)`);
+  db.close();
+};
+
 export default database;
